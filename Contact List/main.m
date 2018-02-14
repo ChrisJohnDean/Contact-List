4b570b5a850c9addd39e7b24ca812504732a5a4e
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-14 09:34:36 -0800

    Added comments to main.m for clarity

--- Contact List/main.m
+++ Contact List/main.m
@@ -16,26 +16,41 @@ int main(int argc, const char * argv[]) {
         
         BOOL dontStop = YES;
         
+        //Instantiate ContactList object
         ContactList *contactList = [[ContactList alloc] init];
         
         while(dontStop) {
+            
+            //Instantiate InputCollector and Contact class
             InputCollector *input = [[InputCollector alloc] init];
             Contact *contact = [[Contact alloc] init];
             
+            //Call inputForPrompt on menu options NSString, and assign users choice to NSString variable
             NSString *menuChoice = [input inputForPrompt:@"\n new - Create a new contact \n list - List all contacts \n quit - Exit application"];
             
+            //Conditions for user's menu choice
             if([menuChoice isEqualToString:@"quit"]) {
+                
                 dontStop = NO;
                 continue;
+                
+            //Prompt user for info and add to array property in ContactList object
             } else if([menuChoice isEqualToString:@"new"]) {
+                
                 contact.name = [input inputForPrompt:@"\n Enter your name"];
                 contact.email = [input inputForPrompt:@"\n Enter your email"];
                 [contactList addContact:contact];
                 NSLog(@"%lu", contactList.contactArray.count);
+            
+            //Display contacts from array property in ContactList object
             } else if([menuChoice isEqualToString:@"list"]) {
+                
                 [contactList displayContacts];
+                
             } else {
+                
                 NSLog(@"Incorrect input, try again");
+                
             }
         }
     }
